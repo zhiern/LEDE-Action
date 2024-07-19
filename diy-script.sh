fbd0a9901b1b1ec830c9728742ea000801f8ec1f
zhiern <168670760+zhiern@users.noreply.github.com>
2024-07-20 02:33:50 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -14,6 +14,7 @@ rm -rf feeds/packages/net/mosdns
 rm -rf feeds/packages/net/msd_lite
 rm -rf feeds/packages/net/smartdns
 rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/themes/luci-theme-argon-mod
 rm -rf feeds/luci/themes/luci-theme-netgear
 rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/luci/applications/luci-app-netdata
