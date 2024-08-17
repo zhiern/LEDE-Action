1c06ed9e22d4ef6c7d7ac06d6537ecc4f6ff8ef8
zhiern <168670760+zhiern@users.noreply.github.com>
2024-08-18 00:39:29 +0800

    Update diy-path.sh

--- diy-path.sh
+++ diy-path.sh
@@ -52,6 +52,7 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages packa
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
 git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
+git clone --depth=1 https://github.com/morytyann/OpenWrt-mihomo package/luci-app-mihomo
 
 # Themes
 git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
